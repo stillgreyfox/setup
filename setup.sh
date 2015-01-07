d74f2ecb0cfc205d8c1031ee4fcae7c8d409af9e
stillgreyfox <me85638251@gmail.com>
2015-01-07 11:47:14 -0500

    finished generalizing master branch

--- setup.sh
+++ setup.sh
@@ -16,34 +16,6 @@ if [ "$branch" != "" ]
   git checkout $branch
 fi
 
-# Install nvm: node-version manager
-# https://github.com/creationix/nvm
-sudo apt-get install -y git curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
-
-# Install jshint to allow checking of JS code within emacs
-# http://jshint.com/
-npm install -g jshint
-
-# Install rlwrap to provide libreadline features with node
-# See: http://nodejs.org/api/repl.html#repl_repl
-sudo apt-get install -y rlwrap
-
-# Install emacs daily snapshot
-# https://launchpad.net/~ubuntu-elisp/+archive/ubuntu/ppa
-sudo add-apt-repository -y ppa:ubuntu-elisp/ppa
-sudo apt-get -qq update
-sudo apt-get install -y emacs-snapshot-nox emacs-snapshot-el emacs-snapshot-common
-
-# Install Heroku toolbelt
-# https://toolbelt.heroku.com/debian
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-
 # git pull and install dotfiles as well
 cd $HOME
 if [ -d ./dotfiles/ ]; then
