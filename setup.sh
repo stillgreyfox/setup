9a08b992aa0411d7bbba368cc1465923f3de7ccc
stillgreyfox <me85638251@gmail.com>
2015-01-07 12:14:20 -0500

    updated so that master only does branch checkouts

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ if [ "$branch" == "" ]; then
   echo "Branch was not specified, please enter branch to use:"
   echo "'workstation' - for new local system setup"
   echo "'ec2' - for new AWS EC2 vm instance setup"
-  echo "'' - defaults to master branch, current / minimal"
+  echo "'' - defaults to master branch, only does branch select"
   read branch
 fi
 if [ "$branch" != "" ]; then
@@ -17,24 +17,3 @@ if [ "$branch" != "" ]; then
   # re-run after checkout
   ./setup.sh
 fi
-
-# git pull and install dotfiles as well
-cd $HOME
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
-fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d.old
-fi
-if [ -d .vim/ ]; then
-    mv .vim .vim.old
-fi
-git clone https://github.com/stillgreyfox/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sb dotfiles/.vimrc .
-ln -sf dotfiles/.emacs.d .
-ln -sf dotfiles/.vim .
-
