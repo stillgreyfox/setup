faebc99ce904429635a44c8fc8b4af118aaa6914
stillgreyfox <me85638251@gmail.com>
2015-01-15 17:51:50 -0500

    added nagging to set git info in master setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
-# Simple setup.sh for configuring new Debian Linux system
+# Simple setup.sh for configuring new Linux system
+# allows user to select git branch for the system type
+
+# let's make the user set their git information so future
+# commits will not bitch about it
+echo -e "Please enter the email you want to tell git\n"
+read email
+echo -e "Please enter the name you want to tell git\n"
+read name
+# set git config with read values
+git config --global user.email "$email"
+git config --global user.name "$name"
 
 # use first argument as branch name (if present)
 branch=$1
 # no branch specified at command line
 if [ "$branch" == "" ]; then
   # ask user for desired branch
-  echo "Branch was not specified, please enter branch to use:"
-  echo "'workstation' - for new local system setup"
-  echo "'ec2' - for new AWS EC2 vm instance setup"
-  echo "'' - defaults to master branch, only does branch select"
+  echo -e "Branch was not specified, please enter branch to use:\n"
+
+  echo -e "  'workstation' - for new local system setup"
+  echo -e "  'ec2'         - for new AWS EC2 vm instance setup"
+  echo -e "  ''            - defaults to master branch, only does branch select\n"
   read branch
 fi
 if [ "$branch" != "" ]; then
