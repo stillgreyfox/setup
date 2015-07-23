774f9777983adb580a2d063193dfa67b56b7ba13
stillgreyfox <me85638251@gmail.com>
2015-07-23 13:04:11 -0400

    Updated README and re-enabled install of dotfiles

--- README.md
+++ README.md
@@ -1,21 +1,17 @@
 setup.git
 =========
-Clone and run this on a new EC2 instance running Ubuntu 12.04.2 LTS to
+Clone and run this on a new workstation running Ubuntu or Debian to
 configure both the machine and your individual development environment as
 follows:
 
 ```sh
 cd $HOME
 sudo apt-get install -y git-core
-git clone https://github.com/startup-class/setup.git
-./setup/setup.sh   
+git clone https://github.com/stillgreyfox/setup.git
+./setup/setup.sh
 ```
 
-See also http://github.com/startup-class/dotfiles and
+See also http://github.com/stillgreyfox/dotfiles and
+http://github.com/startup-class/dotfiles
 [Startup Engineering Video Lectures 4a/4b](https://class.coursera.org/startup-001/lecture/index)
 for more details.
-
-
-
-
-
--- setup.sh
+++ setup.sh
@@ -1,23 +1,59 @@
 #!/bin/bash
 # Simple setup.sh for configuring Ubuntu based workstation
 
-# install some frequently used utlities
-sudo apt-get install screen vim build-essential \
-                     automake autoconf cmake \
-                     libffi-dev libssl-dev\
-                     sshfs autofs \
-                     ipython ipython-doc ipython-notebook \
-                     python-setuptools python-dev python-tk libblas-dev\
-                     curl git subversion meld octave wxmaxima \
-                     gimp inkscape blender xournal
-# these are the packages for pip and virtual env, but may be old
-#sudo apt-get install python-pip python-virtualenv
+# main apps
+sudo apt-get install -y vim xournal meld
+# math apps
+sudo apt-get install -y octave wxmaxima
+# 2d graphics
+sudo apt-get install -y gimp inkscape
+# 3d graphics (commented due to big download)
+#sudo apt-get install -y blender
 
-# for newer versions use python's internal setuptools to get pip,
-# then use pip to get virtualenv
+# system utilities
+sudo apt-get install -y screen sshfs autofs curl git subversion
+# build utilities
+sudo apt-get install -y build-essential gfortran automake autoconf cmake
+# libraries and utils for python pip, numpy / scipy
+sudo apt-get install -y libffi-dev libssl-dev libblas-dev libatlas-base-dev
+sudo apt-get install -y python-dev python-setuptools python-tk
+
+# getting ipython stuff apt, for now, use pip+git for latest (see below)
+# notebook changed names..
+# (will figure out install later, for now package should exist)
+sudo apt-get install ipython ipython-doc ipython-notebook
+
+# for newer versions, use python's internal setuptools to get pip,
+# then use pip to get latest other stuff
+# UPDATE: modified this to also install IPython 2.0
+# using steps from https://theoryl1.wordpress.com/2014/03/16/
+#                  install-ipython-2-0-on-linux-mint-maya/
+sudo easy_install -U distribute
 sudo easy_install pip
 sudo -H pip install -U "requests[security]"
 sudo -H pip install -U pip virtualenv
+#sudo apt-get install pandoc python-pygame
+#sudo -H pip install jinja2 sphinx pyzmq pygments tornado \
+#                    nose pexpect matplotlib jsonschema
+## had to explicitly update tornado (default was v3.x, ipython nb wants 4.x)
+#sudo -H pip install -U tornado
+sudo -H pip install ipython
 
 # git clone and install dotfiles as well
-#./install_dotfiles.sh
+./install_dotfiles.sh
+
+## these were for a python web / db app tutorial on youtube.
+## commenting for now, to be moved to a dr dir with custom scripts...
+#sudo apt-get install pandoc python-pygame
+#sudo -H pip install jinja2 sphinx pyzmq pygments tornado \
+#                 nose pexpect matplotlib jsonschema
+## had to explicitly update tornado (default was v3.x, ipython nb wants 4.x)
+#sudo -H pip install -U tornado
+## NOTE: building scipy and the kits takes 30 ish mins!
+#sudo -H pip install scipy scikit-image scikit-learn
+## tried to get the master branch of ipython, but didn't run, used pip instead
+##cd $HOME
+##git clone --recursive https://github.com/ipython/ipython.git
+##cd ipython
+##sudo python setup.py install
+#sudo -H pip install ipython
