7d163fc1fe5e3e315e5beedb163e99d205114278
Tom Gambone II <tom@narlson.com>
2015-10-03 17:37:30 -0400

    Automatically update --init submodules in branch

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -9,8 +9,12 @@ cd $HOME
 if [ -d ./dotfiles/ ]; then
     mv ./dotfiles ./dotfiles.old
 fi
+# this one only works if your master has submodules, after a checkout
+# we have to explicitly update that branch's submodules
+#git clone --recursive https://github.com/stillgreyfox/dotfiles.git
 git clone https://github.com/stillgreyfox/dotfiles.git
 cd dotfiles
 git checkout $SETUP_BRANCH
+git submodule update --init
 ./setup.sh
 cd ../setup
